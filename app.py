86b110327c51c926b2db69d627bee591e09c44de
nangelsoto <122932482+nangelsoto@users.noreply.github.com>
2024-04-17 22:42:25 -0500

    app.py

--- app.py
+++ app.py
@@ -3,27 +3,32 @@ import cv2
 import numpy as np
 import pytesseract
 from PIL import Image
+from googletrans import Translator
 
+translator = Translator()
 
 st.title("Reconocimiento óptico de Caracteres")
 
 img_file_buffer = st.camera_input("Toma una Foto")
 
 with st.sidebar:
-      filtro = st.radio("Aplicar Filtro",('Con Filtro', 'Sin Filtro'))
-
+    filtro = st.radio("Aplicar Filtro", ('Con Filtro', 'Sin Filtro'))
 
 if img_file_buffer is not None:
     # To read image file buffer with OpenCV:
     bytes_data = img_file_buffer.getvalue()
     cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
-    
+
     if filtro == 'Con Filtro':
-         cv2_img=cv2.bitwise_not(cv2_img)
+        cv2_img = cv2.bitwise_not(cv2_img)
     else:
-         cv2_img= cv2_img
-    
-        
+        cv2_img = cv2_img
+
     img_rgb = cv2.cvtColor(cv2_img, cv2.COLOR_BGR2RGB)
-    text=pytesseract.image_to_string(img_rgb)
-    st.write(text) 
+    text = pytesseract.image_to_string(img_rgb)
+
+    # Translate the recognized text
+    translated_text = translator.translate(text, dest='en').text
+    st.write("Texto original:", text)
+    st.write("Texto traducido:", translated_text)
+
