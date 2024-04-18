57dd13137a19851736a6d81803cccbe0a0dd6c42
nangelsoto <122932482+nangelsoto@users.noreply.github.com>
2024-04-17 22:46:53 -0500

    app.py

--- app.py
+++ app.py
@@ -7,28 +7,29 @@ from googletrans import Translator
 
 translator = Translator()
 
+# Configurar la ruta de Tesseract
+pytesseract.pytesseract.tesseract_cmd = '/usr/bin/tesseract'  # Ruta a la instalación de Tesseract en tu sistema
+
 st.title("Reconocimiento óptico de Caracteres")
 
-img_file_buffer = st.camera_input("Toma una Foto")
+img_file_buffer = st.file_uploader("Toma una Foto", type=["png", "jpg", "jpeg"])
 
 with st.sidebar:
     filtro = st.radio("Aplicar Filtro", ('Con Filtro', 'Sin Filtro'))
 
 if img_file_buffer is not None:
-    # To read image file buffer with OpenCV:
-    bytes_data = img_file_buffer.getvalue()
-    cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
+    # To read image file buffer with PIL:
+    image = Image.open(img_file_buffer)
+    img_rgb = np.array(image)
 
     if filtro == 'Con Filtro':
-        cv2_img = cv2.bitwise_not(cv2_img)
-    else:
-        cv2_img = cv2_img
+        img_rgb = cv2.bitwise_not(img_rgb)
 
-    img_rgb = cv2.cvtColor(cv2_img, cv2.COLOR_BGR2RGB)
     text = pytesseract.image_to_string(img_rgb)
 
     # Translate the recognized text
     translated_text = translator.translate(text, dest='en').text
     st.write("Texto original:", text)
     st.write("Texto traducido:", translated_text)
+
 
